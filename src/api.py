84c15477b723e3ca9398f96e647d52d0556a5969
Clarence Castillo <clarencecastillo@outlook.com>
2020-06-01 23:05:52 +0800

    Disable SSL verification for all requests

--- src/api.py
+++ src/api.py
@@ -94,7 +94,7 @@ class PowerThesaurus:
         if not term_id:
             return []
         params = self.build_thesaurus_query_params(term_id, query_type)
-        r = requests.post(self.api_url, json=params, headers=self.request_headers)
+        r = requests.post(self.api_url, json=params, headers=self.request_headers, verify=False)
         self.logger.debug('thesaurus_query: {} {}'.format(r.status_code, r.url))
         r.raise_for_status()
         return self.parse_thesaurus_query_response(r.json())
@@ -108,7 +108,7 @@ class PowerThesaurus:
 
     def search_query(self, query):
         params = self.build_search_query_params(query)
-        r = requests.post(self.api_url, json=params, headers=self.request_headers)
+        r = requests.post(self.api_url, json=params, headers=self.request_headers, verify=False)
         self.logger.debug('search_query: {} {}'.format(r.status_code, r.url))
         r.raise_for_status()
         return self.parse_search_query_response(r.json())
