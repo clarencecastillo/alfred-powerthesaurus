3b541c4526d46e1661b071a04149a669188ab157
Giovanni <5774411+giovannicoppola@users.noreply.github.com>
2022-03-23 17:40:09 -0400

    Migrating to Python 3

---
+++ src/lib/_deleteMaybe/tld/registry.py
@@ -0,0 +1,12 @@
+import warnings
+from .base import Registry
+
+__author__ = "Artur Barseghyan"
+__copyright__ = "2013-2021 Artur Barseghyan"
+__license__ = "MPL-1.1 OR GPL-2.0-only OR LGPL-2.1-or-later"
+__all__ = ("Registry",)
+
+warnings.warn(
+    "The `Registry` class is moved from `tld.registry` to `tld.base`.",
+    DeprecationWarning,
+)
